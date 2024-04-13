4ec9f0506f83b2084a61c524e6ee07459bc0bc2c
Celia <celia.letellier@gmail.com>
2024-04-13 16:23:58 +0200

    mise en ligne application pays du monde

---
+++ src/pages/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque excepturi
+        repudiandae consequuntur illo ullam, iste quae enim libero quasi sequi
+        tempore sunt incidunt exercitationem delectus suscipit, at numquam
+        provident doloremque.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae
+        aspernatur laudantium velit recusandae quidem labore quaerat consequatur
+        perferendis dolorum dignissimos error voluptates, veritatis doloribus at
+        mollitia doloremque eaque explicabo facilis placeat obcaecati pariatur
+        incidunt commodi? Ducimus obcaecati magnam placeat, facilis nostrum
+        aperiam asperiores amet ipsum quae in eius quo inventore, suscipit ut et
+        quaerat ab nobis vero? Tenetur voluptatem non amet corporis iusto
+        adipisci molestiae fuga accusantium nisi suscipit itaque id, in sapiente
+        dicta libero tempora. Dolore provident repellat eligendi voluptatibus ad
+        recusandae nobis sint dolorem doloremque nostrum. Ipsam voluptatibus
+        fugiat quasi rerum laudantium, perferendis labore quisquam natus ab.
+      </p>
+    </div>
+  );
+};
+
+export default About;
